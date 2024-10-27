4066dfc58365109b37de0c3a0a577b6a885397d3
Thabiso Phefo <phefothabiso@gmail.com>
2024-10-27 02:10:04 +0200

    Your commit message

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from  '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
-  const loggedIn = { firstName: 'Thabiso'};
+  const loggedIn = { firstName: 'Thabiso', lastName: 'Phefo', email: 'Phefothabiso.gmail.com'};
 
   return (
     <section className="home">
@@ -21,7 +22,15 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[]}
+      />
     </section>
   )
 }
